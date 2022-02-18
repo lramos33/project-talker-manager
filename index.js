f14e2c059fd95db19e6244796c175c69f514e2e2
Leonardo Ramos <oliveira.leonardo3004@gmail.com>
2022-02-18 17:35:27 -0300

    🛠 req01

--- index.js
+++ index.js
@@ -1,13 +1,26 @@
 const express = require('express');
-const bodyParser = require('body-parser');
+const fs = require('fs/promises');
 
 const app = express();
-app.use(bodyParser.json());
 
 const HTTP_OK_STATUS = 200;
+const HTTP_NOT_FOUND_STATUS = 404;
 const PORT = '3000';
+const TALKERS_FILE = './talker.json';
 
-// não remova esse endpoint, e para o avaliador funcionar
+// <-- REQUISITO 01 -->
+app.get('/talker', async (_req, res, _next) => {
+  const talkers = JSON.parse(await fs.readFile(TALKERS_FILE));
+  if (talkers.length === 0) return res.status(HTTP_OK_STATUS).send([]);
+  res.status(HTTP_OK_STATUS).json(talkers);
+});
+
+// <-- REQUISITO 02 -->
+app.get('/talker:id', () => {
+
+});
+
+// <-- ENDPOINT AVALIADOR -->
 app.get('/', (_request, response) => {
   response.status(HTTP_OK_STATUS).send();
 });
