bc7865dccc590ee070812e907fd43c513bbce03f
Leonardo Ramos <oliveira.leonardo3004@gmail.com>
2022-02-18 19:20:31 -0300

    🛠 req02

--- index.js
+++ index.js
@@ -16,8 +16,12 @@ app.get('/talker', async (_req, res, _next) => {
 });
 
 // <-- REQUISITO 02 -->
-app.get('/talker:id', () => {
-
+app.get('/talker/:id', async (req, res, next) => {
+  const { id } = req.params;
+  const talkers = JSON.parse(await fs.readFile(TALKERS_FILE));
+  const talker = talkers.find((talker) => talker.id === parseInt(id));
+  if (!talker) return res.status(HTTP_NOT_FOUND_STATUS).json({ message: "Pessoa palestrante não encontrada" });
+  res.status(HTTP_OK_STATUS).json(talker)
 });
 
 // <-- ENDPOINT AVALIADOR -->
